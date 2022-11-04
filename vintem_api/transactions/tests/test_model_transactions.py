b2cb88c6d87ab30dc36f21f424b8285b10c92e5a
Matheus Lopes <mlopes.matheus@gmail.com>
2022-11-03 21:58:27 -0300

    fix test

--- vintem_api/transactions/tests/test_model_transactions.py
+++ vintem_api/transactions/tests/test_model_transactions.py
@@ -1,19 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from vintem_api.transactions.models import Transaction
 
 
-class TransactionModelTest(TestCase):    
+class TransactionModelTest(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(username='user_1',
+                                                         password='pass123',
+                                                         email='user_1@email.com')
+
+        # self.user.save()
+        # user = authenticate(username='user_1', password='pass123')
         self.obj = Transaction(
             type=Transaction.TransactionType.INCOME,
             description='descrição teste',
-            value='12.34'
+            value='12.34',
+            owner=self.user,
         )
         self.obj.save()
 
-    
-    # def test_create(self):
-    #     self.assertTrue(Transaction.objects.exists())
+    def tearDown(self):
+        self.user.delete()
 
-
+    def test_create(self):
+        self.assertTrue(Transaction.objects.exists())
--- vintem_api/transactions/urls.py
+++ vintem_api/transactions/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'transactions'
 
-urlpatterns = [    
+urlpatterns = [
     path('', views.TransactionList.as_view(), name='transaction-list'),
     path('<int:pk>/', views.TransactionDetail.as_view(), name='transaction-detail'),
     path('user/', views.UserList.as_view(), name='user-list'),
@@ -15,4 +15,4 @@ urlpatterns += [
     path('api-auth', include('rest_framework.urls')),
 ]
 
-#urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
