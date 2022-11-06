f8874444f6bf71923f715498bd98a6918ec5ec0c
Matheus Lopes <mlopes.matheus@gmail.com>
2022-11-06 19:50:11 -0300

    add test

--- vintem_api/transactions/tests/test_api_transactions.py
+++ vintem_api/transactions/tests/test_api_transactions.py
@@ -4,10 +4,24 @@ from unittest import skip
 
 from django.contrib.auth.models import User
 from django.urls import reverse
+
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 from vintem_api.transactions.models import Transaction
+
+TRANSACTION_CREATE_LIST_URL = reverse('transactions:transaction-list')
+
+
+def create_transaction(self, description='test description', value=1,
+                       transaction_type=Transaction.TransactionType.EXPENSE):
+    url = TRANSACTION_CREATE_LIST_URL
+    data = {
+        "description": description,
+        "value": value,
+        "type": transaction_type
+    }
+    return self.client.post(url, data)
 
 
 class TransactionAPITest(APITestCase):
@@ -17,14 +31,9 @@ class TransactionAPITest(APITestCase):
         self.client = APIClient()
 
         self.client.login(username='user_1', password='user_1_password')
+        self.transaction_1 = create_transaction(self, 'test description 1', 10, Transaction.TransactionType.EXPENSE) \
+            .data
 
-        url = reverse('transactions:transaction-list')
-        data = {
-            "description": "test description 1",
-            "value": 11,
-            "type": Transaction.TransactionType.EXPENSE
-        }
-        self.client.post(url, data)
         self.client.logout()
 
     def tearDown(self):
@@ -34,7 +43,7 @@ class TransactionAPITest(APITestCase):
         """
         Ensure we can get a transaction object.
         """
-        url = reverse('transactions:transaction-detail', args=[self.user_1.id])
+        url = reverse('transactions:transaction-detail', args=[self.transaction_1.get('id')])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -46,28 +55,15 @@ class TransactionAPITest(APITestCase):
         Ensure we can list all logged user transactions
         """
         self.client.login(username='user_2', password='user_2_password')
-        url = reverse('transactions:transaction-list')
-        data = {
-            "description": "test description 2",
-            "value": 12,
-            "type": Transaction.TransactionType.EXPENSE
-        }
-        self.client.post(url, data)
+        create_transaction(self, 'test description 2', 12, Transaction.TransactionType.EXPENSE)
         self.client.logout()
 
         self.client.login(username='user_1', password='user_1_password')
-        url = reverse('transactions:transaction-list')
-        data = {
-            "description": "test description 3",
-            "value": 13,
-            "type": Transaction.TransactionType.INCOME
-        }
-        self.client.post(url, data)
+        create_transaction(self, 'test description 3', 13, Transaction.TransactionType.EXPENSE)
 
-        response = self.client.get(url)
-
+        response = self.client.get(TRANSACTION_CREATE_LIST_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Transaction.objects.count(), 3)
+        self.assertEqual(response.data.get('count'), 2)
 
     @skip("")
     def test_expenses_sum(self):
@@ -75,13 +71,7 @@ class TransactionAPITest(APITestCase):
         Ensure we can Sum all user expense transactions
         """
         self.client.login(username='user_1', password='user_1_password')
-        url = reverse('transactions:transaction-list')
-        data = {
-            "description": "test description 2",
-            "value": 1313.13,
-            "type": Transaction.TransactionType.EXPENSE
-        }
-        self.client.post(url, data)
+        create_transaction(self, 'test description 4', 14, Transaction.TransactionType.EXPENSE)
 
         url = reverse('transactions:transaction-closing')
         data = {
