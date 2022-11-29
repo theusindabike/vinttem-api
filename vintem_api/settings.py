421976ae3a9db7fee95464e5de8eb4c318847043
Matheus Lopes <mlopes.matheus@gmail.com>
2022-11-29 20:59:43 -0300

    add ssl

--- vintem_api/settings.py
+++ vintem_api/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
